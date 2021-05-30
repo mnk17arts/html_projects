d68a22de1d68c90c894f07abe77f7ea8e31a9e76
Madhapur Nitish Kumar <71878747+mnk17arts@users.noreply.github.com>
2021-05-30 19:09:48 +0530

    Update roman-numeral-converter.js

--- JS DSA Projects/roman-numeral-converter.js
+++ JS DSA Projects/roman-numeral-converter.js
@@ -1,4 +1,4 @@
-/* ROMAN NUMERAL CONVERTER...
+/* ROMAN NUMERAL CONVERTER... max value: 3999
 Convert the given number into a roman numeral.
 
 All roman numerals[https://www.mathsisfun.com/roman-numerals.html] answers should be provided in upper-case.
