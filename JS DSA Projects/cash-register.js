43f16eaa7a24582223edc60ec9177f70dd45d18e
Madhapur Nitish Kumar <71878747+mnk17arts@users.noreply.github.com>
2021-05-31 14:36:17 +0530

    Create cash-register.js

---
+++ JS DSA Projects/cash-register.js
@@ -0,0 +1,98 @@
+/* CASH REGISTER...
+Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), 
+payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+
+cid is a 2D array listing available currency.
+
+The checkCashRegister() function should always return an object with a status key and a change key.
+
+Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+
+Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+
+Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
+
+Currency Unit	Amount
+Penny	$0.01 (PENNY)
+Nickel	$0.05 (NICKEL)
+Dime	$0.1 (DIME)
+Quarter	$0.25 (QUARTER)
+Dollar	$1 (ONE)
+Five Dollars	$5 (FIVE)
+Ten Dollars	$10 (TEN)
+Twenty Dollars	$20 (TWENTY)
+One-hundred Dollars	$100 (ONE HUNDRED)
+
+*/
+
+function checkCashRegister(price, cash, cid){
+  const TYPES = {
+    "PENNY": .01,
+    "NICKEL": .05,
+    "DIME": .10,
+    "QUARTER": .25,
+    "ONE": 1.00,
+    "FIVE": 5.00,
+    "TEN": 10.00,
+    "TWENTY": 20.00,
+    "ONE HUNDRED": 100.00
+  }
+  let cidsum = 0;
+  for (let element of cid) {
+    cidsum += element[1];
+  }
+  cidsum = cidsum.toFixed(2);
+  let changeToGive = cash - price;
+  const changeArray = [];
+  if (changeToGive > cidsum) {
+    return { status: "INSUFFICIENT_FUNDS", change: changeArray };
+  } else if (changeToGive.toFixed(2) === cidsum) {
+    return { status: "CLOSED", change: cid };
+  } else {
+    cid = cid.reverse();
+    for (let elem of cid) {
+      let temp = [elem[0], 0];
+      while (changeToGive >= TYPES[elem[0]] && elem[1] > 0) {
+        temp[1] += TYPES[elem[0]];
+        elem[1] -= TYPES[elem[0]];
+        changeToGive -= TYPES[elem[0]];
+        changeToGive = changeToGive.toFixed(2);
+      }
+      if (temp[1] > 0) {
+        changeArray.push(temp);
+      }
+    }
+  }
+  if (changeToGive > 0) {
+    return { status: "INSUFFICIENT_FUNDS", change: [] };
+  }
+  return { status: "OPEN", change: changeArray};
+}
+
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55],
+                             ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+
+/*  other testcases...
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20],
+["TWENTY", 60], ["ONE HUNDRED", 100]])
+should return an object.
+
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], 
+["TWENTY", 60], ["ONE HUNDRED", 100]])
+should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+
+checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], 
+["TWENTY", 60], ["ONE HUNDRED", 100]])
+should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], 
+["ONE HUNDRED", 0]]) should return 
+{status: "INSUFFICIENT_FUNDS", change: []}.
+
+checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], 
+["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
+
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], 
+["ONE HUNDRED", 0]]) should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], 
+["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
+*/
